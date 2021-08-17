95a9c460cb9cc3d3a6c64426c77263a5255a7607
JorayDan <33612991+JorayDan@users.noreply.github.com>
2021-08-17 09:35:06 +0800

    Add files via upload

---
+++ wordcount.py
@@ -0,0 +1,34 @@
+# 导入扩展库
+import re # 正则表达式库
+import collections # 词频统计库
+import numpy as np # numpy数据处理库
+import jieba # 结巴分词
+
+#读取txt文件
+ftxt = open('数据安全法.txt',encoding='utf-8',errors='ignore') # 打开文件
+string_data = ftxt.read() # 读出整个文件
+ftxt.close() # 关闭文件
+
+# 文本预处理
+pattern = re.compile(u'\t|\n|\.|-|:|;|\)|\(|\?|"') # 定义正则表达式匹配模式
+string_data = re.sub(pattern, '', string_data) # 将符合模式的字符去除
+
+# 文本分词
+seg_list_exact = jieba.cut(string_data, cut_all = False) # 精确模式分词
+object_list = []
+remove_words = [u'的', u'，',u'和', u'是', u'随着', u'对于', u'对',u'等',u'能',u'都',u'。',u' ',u'、',u'中',u'在',u'了',
+                u'通常',u'如果',u'我们',u'需要'] # 自定义去除词库
+
+for word in seg_list_exact: # 循环读出每个分词
+    if word not in remove_words: # 如果不在去除词库中
+        object_list.append(word) # 分词追加到列表
+
+# 词频统计
+word_counts = collections.Counter(object_list) # 对分词做词频统计
+word_counts_top15 = word_counts.most_common(15) # 获取前15最高频的词
+print (word_counts_top15) # 输出检查
+
+for i in word_counts_top15:
+    f = open('D:/Py/file.txt', 'a')
+    f.write(str(i) + '\n')
+    f.close()
